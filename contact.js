ae0f0db037d294a883c6444ddaf98c2d28765849
Shandesh Chaudhary <hancyshandeshc@gmail.com>
2026-09-20 11:46:43 +0545

    Delete contact.js

--- contact.js
+++
@@ -1,2 +0,0 @@
-const contactForm=document.getElementById("contactForm");
-if(contactForm){contactForm.addEventListener("submit",e=>{e.preventDefault();document.getElementById("contactSuccess").hidden=false;contactForm.reset();});}
